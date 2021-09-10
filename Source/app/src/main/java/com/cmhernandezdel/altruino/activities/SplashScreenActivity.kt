17e05d3f7604767658f1254562179f6545b58d1c
cmhernandezdel <cmhernandezdel@gmail.com>
2021-09-10 22:40:11 +0200

    Working splash screen.

--- Source/app/src/main/java/com/cmhernandezdel/altruino/activities/SplashScreenActivity.kt
+++ Source/app/src/main/java/com/cmhernandezdel/altruino/activities/SplashScreenActivity.kt
@@ -1,12 +1,24 @@
 package com.cmhernandezdel.altruino.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.cmhernandezdel.altruino.R
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val handler = Handler(Looper.getMainLooper())
+    private val delayInMilliseconds = 1000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        handler.postDelayed({
+            val homeIntent = Intent(this, MainActivity::class.java)
+            startActivity(homeIntent)
+            finish()
+        }, delayInMilliseconds)
     }
 }
