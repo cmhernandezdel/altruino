e9b6098d14981d3397c168859c989f2c5cc5c083
cmhernandezdel <cmhernandezdel@gmail.com>
2021-11-23 19:34:15 +0100

    Fix movement of the robot.

--- Source/app/src/main/java/com/cmhernandezdel/altruino/activities/MainActivity.kt
+++ Source/app/src/main/java/com/cmhernandezdel/altruino/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cmhernandezdel.altruino.activities
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Button
 import com.cmhernandezdel.altruino.R
 import com.cmhernandezdel.altruino.modules.BluetoothModule
@@ -10,10 +12,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : Activity() {
-    private val upSignal = "F"
-    private val downSignal = "B"
+    private val forwardSignal = "F"
+    private val backwardsSignal = "B"
     private val leftSignal = "L"
     private val rightSignal = "R"
+    private val stopSignal = "\u0000"
     private val bluetoothModule = BluetoothModule
 
     var buttonUp: Button? = null
@@ -22,6 +25,7 @@ class MainActivity : Activity() {
     var buttonRight: Button? = null
     var coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,35 +35,62 @@ class MainActivity : Activity() {
         buttonLeft = findViewById(R.id.button_left)
         buttonRight = findViewById(R.id.button_right)
 
-        buttonUp?.setOnClickListener { onUpButtonClick() }
-        buttonDown?.setOnClickListener { onDownButtonClick() }
-        buttonLeft?.setOnClickListener { onLeftButtonClick() }
-        buttonRight?.setOnClickListener { onRightButtonClick() }
+        buttonUp?.setOnTouchListener { _, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                onButtonPress(forwardSignal)
+                true
+            }
+            else if(event.action == MotionEvent.ACTION_UP){
+                onButtonRelease()
+                true
+            }
+            else false
+        }
+
+        buttonDown?.setOnTouchListener { _, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                onButtonPress(backwardsSignal)
+                true
+            }
+            else if(event.action == MotionEvent.ACTION_UP){
+                onButtonRelease()
+                true
+            }
+            else false
+        }
+
+        buttonLeft?.setOnTouchListener { _, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                onButtonPress(leftSignal)
+                true
+            }
+            else if(event.action == MotionEvent.ACTION_UP){
+                onButtonRelease()
+                true
+            }
+            else false
+        }
+
+        buttonRight?.setOnTouchListener { _, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                onButtonPress(rightSignal)
+                true
+            }
+            else if(event.action == MotionEvent.ACTION_UP){
+                onButtonRelease()
+                true
+            }
+            else false
+        }
 
         bluetoothModule.setContext(this)
     }
 
-    private fun onUpButtonClick() = coroutineScope.launch {
-        bluetoothModule?.let { module ->
-            val success = module.sendSignalAsync(upSignal)
-        }
+    private fun onButtonPress(signal: String) = coroutineScope.launch {
+        bluetoothModule.sendSignalAsync(signal)
     }
 
-    private fun onDownButtonClick() = coroutineScope.launch {
-        bluetoothModule?.let { module ->
-            val success = module.sendSignalAsync(downSignal)
-        }
-    }
-
-    private fun onLeftButtonClick() = coroutineScope.launch {
-        bluetoothModule?.let { module ->
-            val success = module.sendSignalAsync(leftSignal)
-        }
-    }
-
-    private fun onRightButtonClick() = coroutineScope.launch {
-        bluetoothModule?.let { module ->
-            val success = module.sendSignalAsync(rightSignal)
-        }
+    private fun onButtonRelease() = coroutineScope.launch {
+        bluetoothModule.sendSignalAsync(stopSignal)
     }
 }
