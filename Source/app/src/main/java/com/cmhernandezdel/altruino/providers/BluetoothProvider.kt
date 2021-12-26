95295d9bcc501655ad7b4ff060b535798eb088e7
cmhernandezdel <cmhernandezdel@gmail.com>
2021-12-26 13:35:25 +0100

    Implement bluetooth turn on method.

--- Source/app/src/main/java/com/cmhernandezdel/altruino/providers/BluetoothProvider.kt
+++ Source/app/src/main/java/com/cmhernandezdel/altruino/providers/BluetoothProvider.kt
@@ -1,11 +1,36 @@
 package com.cmhernandezdel.altruino.providers
 
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.cmhernandezdel.altruino.exceptions.BluetoothNotAvailableException
+import dagger.hilt.android.qualifiers.ApplicationContext
 
-class BluetoothProvider : IBluetoothStatusProvider, IBluetoothConnectionProvider {
+class BluetoothProvider(@ApplicationContext context: Context) : IBluetoothStatusProvider, IBluetoothConnectionProvider {
+    private val classTag = "BluetoothProvider.kt"
+    private val bluetoothBroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                    val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
+                    when (state) {
+                        BluetoothAdapter.STATE_OFF -> Log.i(classTag, "Bluetooth turned off")
+                        BluetoothAdapter.STATE_ON -> Log.i(classTag, "Bluetooth turned on")
+                    }
+                }
+            }
+        }
+
+    }
+
     override fun enableBluetooth() {
-        TODO("Not yet implemented")
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter() ?: throw BluetoothNotAvailableException("This device has no bluetooth adapter")
+        val isTurningOn = bluetoothAdapter.enable()
+        if (!isTurningOn) throw BluetoothNotAvailableException("Bluetooth could not turn on")
     }
 
     override fun disableBluetooth() {
