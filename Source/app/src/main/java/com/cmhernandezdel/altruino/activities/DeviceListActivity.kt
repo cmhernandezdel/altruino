e9aed7d4f7581b879fdda189047874da165972b2
cmhernandezdel <cmhernandezdel@gmail.com>
2022-03-06 00:33:02 +0100

    Switch view pager for view pager 2.

--- Source/app/src/main/java/com/cmhernandezdel/altruino/activities/DeviceListActivity.kt
+++ Source/app/src/main/java/com/cmhernandezdel/altruino/activities/DeviceListActivity.kt
@@ -4,13 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.TextView
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.cmhernandezdel.altruino.R
 import com.cmhernandezdel.altruino.adapters.DeviceListActivityPagerAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 // to pair: 1234
 // 9600 bps
@@ -20,19 +19,17 @@ class DeviceListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device_list)
 
-        val pagerAdapter = DeviceListActivityPagerAdapter(supportFragmentManager, applicationContext)
+        val pagerAdapter = DeviceListActivityPagerAdapter(this)
         val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
-        val viewPager = findViewById<ViewPager>(R.id.pager)
+        val viewPager = findViewById<ViewPager2>(R.id.pager)
         viewPager.adapter = pagerAdapter
-        tabLayout.setupWithViewPager(viewPager)
-
-        /*
-        val deviceListView = findViewById<ListView>(R.id.bluetooth_devices_list)
-        val list = ArrayList<String>()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-        deviceListView.adapter = adapter
-        deviceListView.onItemClickListener = onDeviceClicked
-        */
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.view_pager_title_status)
+                1 -> getString(R.string.view_pager_title_available)
+                else -> getString(R.string.view_pager_title_bonded)
+            }
+        }.attach()
     }
 
     private val onDeviceClicked = AdapterView.OnItemClickListener { _, view, _, _ ->
--- Source/app/src/main/java/com/cmhernandezdel/altruino/adapters/DeviceListActivityPagerAdapter.kt
+++ Source/app/src/main/java/com/cmhernandezdel/altruino/adapters/DeviceListActivityPagerAdapter.kt
@@ -1,34 +1,22 @@
 package com.cmhernandezdel.altruino.adapters
 
-import android.content.Context
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.cmhernandezdel.altruino.R
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.cmhernandezdel.altruino.fragments.AvailableDevicesFragment
 import com.cmhernandezdel.altruino.fragments.BluetoothStatusFragment
 import com.cmhernandezdel.altruino.fragments.BondedDevicesFragment
 
-class DeviceListActivityPagerAdapter(manager: FragmentManager, private val context: Context) :
-    FragmentPagerAdapter(manager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    override fun getCount(): Int {
+class DeviceListActivityPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
         return 3
     }
 
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> BluetoothStatusFragment()
             1 -> AvailableDevicesFragment()
             else -> BondedDevicesFragment()
         }
     }
-
-    override fun getPageTitle(position: Int): CharSequence {
-        return when(position) {
-            0 -> context.getString(R.string.view_pager_title_status)
-            1 -> context.getString(R.string.view_pager_title_available)
-            else -> context.getString(R.string.view_pager_title_bonded)
-        }
-    }
-
 }
