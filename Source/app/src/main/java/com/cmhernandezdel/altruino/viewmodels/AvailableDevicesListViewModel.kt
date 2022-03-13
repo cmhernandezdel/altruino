6c95553d08bc348d6fce3704e00242199b162571
cmhernandezdel <cmhernandezdel@gmail.com>
2022-03-13 11:30:41 +0100

    Get available devices in recycler view.

--- Source/app/src/main/java/com/cmhernandezdel/altruino/viewmodels/AvailableDevicesListViewModel.kt
+++ Source/app/src/main/java/com/cmhernandezdel/altruino/viewmodels/AvailableDevicesListViewModel.kt
@@ -1,11 +1,11 @@
 package com.cmhernandezdel.altruino.viewmodels
 
 import android.bluetooth.BluetoothDevice
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.cmhernandezdel.altruino.providers.IBluetoothConnectionProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,17 +13,28 @@ import javax.inject.Inject
 @HiltViewModel
 class AvailableDevicesListViewModel @Inject constructor(private val bluetoothConnectionProvider: IBluetoothConnectionProvider) : ViewModel() {
 
-    val availableDevices: Flow<BluetoothDeviceViewModel>
+    private val availableDevicesFlow: Flow<BluetoothDeviceViewModel>
+    private val _devices = MutableLiveData<ArrayList<BluetoothDeviceViewModel>>(arrayListOf())
+    val devices: LiveData<ArrayList<BluetoothDeviceViewModel>> by this::_devices
 
     init {
         startBluetoothDiscovery()
         val viewModelFlow = bluetoothConnectionProvider.getAvailableDevicesAsFlow().transform<BluetoothDevice, BluetoothDeviceViewModel> {
             BluetoothDeviceViewModel(it)
         }
-        availableDevices = viewModelFlow
+        availableDevicesFlow = viewModelFlow
     }
 
     private fun startBluetoothDiscovery() = viewModelScope.launch {
         bluetoothConnectionProvider.startBluetoothDiscovery()
+        availableDevicesFlow.collect {
+            if (_devices.value != null) {
+                val list = _devices.value!!
+                list.add(it)
+                _devices.postValue(list)
+            } else {
+                _devices.postValue(arrayListOf(it))
+            }
+        }
     }
 }
